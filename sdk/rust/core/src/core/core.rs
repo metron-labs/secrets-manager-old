ab2c5154e9a4e08a9859dae7890bc22bfb25a32d
prathmeshv-metron <prathmesh.vagare@metronlabs.com>
2025-05-21 18:54:03 +0530

    fix create folder return value

--- sdk/rust/core/src/core/core.rs
+++ sdk/rust/core/src/core/core.rs
@@ -1621,7 +1621,7 @@ impl SecretsManager {
             shared_folder.folder_key.clone(),
         )?;
         let _resp = self.post_query("create_folder".to_string(), &payload)?;
-        Ok(format!("created folder :{}", payload.folder_uid))
+        Ok(payload.folder_uid)
     }
 
     pub fn get_secret_by_title(&mut self, title: &str) -> Result<Option<Vec<Record>>, KSMRError> {
